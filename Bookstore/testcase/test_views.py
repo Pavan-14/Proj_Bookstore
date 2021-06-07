b01f578f837b8441532c9cf61cd18e53de78d64c
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 17:16:33 +0530

    skfksdlkfj

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -1,9 +1,10 @@
 # from django.contrib.auth.models import AnonymousUser, User
 # from cuser.views import user_signingit 
+import unittest
 from django.test import TestCase
 from django.test import Client
 
-class ViewsTestCase(TestCase):
+class ViewsTestCase(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
@@ -21,5 +22,8 @@ class ViewsTestCase(TestCase):
     #     request = self.factory.get('http://3.85.22.38:8080/cuser/signin/')
     #     request.user = AnonymousUser()
     #     response = user_signin(request)
+
+if __name__=="__main__":
+    unittest.main()
 
     #     self.assertNotEqual(response.status_code, 404, 'status code not 404')
