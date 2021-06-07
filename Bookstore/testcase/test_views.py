71d5e2f5541254b4742863650c2ccd06c237b4b7
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 15:54:45 +0530

    modified the testcases

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -1,13 +1,26 @@
-from unittest import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
+from django.test import Client
 
 class ViewsTestCase(TestCase):
-    
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        self.factory = RequestFactory()
+   
     def test_index_loads_properly(self):
         """The index page loads properly"""
         response = self.client.get('http://3.85.22.38:8080/books/')
         self.assertEqual(response.status_code, 200)
-    
+  
     def test_login(self):
         response = self.client.get('http://3.85.22.38:8080/cuser/signin/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cuser/signin.html')
+
+    def test_login(self):
+        request = self.factory.get('http://3.85.22.38:8080/cuser/signin/')
+        request.user = AnonymousUser()
+        response = my_view(request)
+
+        self.assertNotEqual(response.status_code, 404, 'status code not 404')
