c701c14354a1fc7847963de7622bb5d1c1ca51c1
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 14:03:30 +0530

    @classmethod

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 
 class ViewsTestCase(TestCase):
+    @classmethod
     def test_index_loads_properly(self):
         """The index page loads properly"""
         response = self.client.get('http://*.*.*.*:8080/books/')
         self.assertEqual(response.status_code, 200)
-
+    @classmethod
     def test_login(self):
         response = self.client.get('http://*.*.*.*:8080/cuser/signin/')
         self.assertEqual(response.status_code, 200)
