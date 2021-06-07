ab515062a34d5fbfd81a8026ba638392754211c8
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 14:29:47 +0530

    changes

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -1,13 +1,12 @@
 from django.test import TestCase
 
 class ViewsTestCase(TestCase):
-    @classmethod
+    
     def test_index_loads_properly(self):
         """The index page loads properly"""
         response = self.client.get('http://3.85.22.38:8080/books/')
         self.assertEqual(response.status_code, 200)
     
-    @classmethod
     def test_login(self):
         response = self.client.get('http://3.85.22.38:8080/cuser/signin/')
         self.assertEqual(response.status_code, 200)
