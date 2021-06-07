0bceb308fc0b36f53e4ddce897bf5dd1e4aeb955
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 13:37:01 +0530

    ip changed in testcase

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -3,10 +3,10 @@ from django.test import TestCase
 class ViewsTestCase(TestCase):
     def test_index_loads_properly(self):
         """The index page loads properly"""
-        response = self.client.get('http://127.0.0.1:8000/books/')
+        response = self.client.get('http://3.85.22.38:8080/books/')
         self.assertEqual(response.status_code, 200)
 
     def test_login(self):
-        response = self.client.get('http://127.0.0.1:8000/cuser/signin/')
+        response = self.client.get('http://3.85.22.38:8080/cuser/signin/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cuser/signin.html')
