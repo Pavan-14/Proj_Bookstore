e2281706e0c9f864f2c01096394f76c2d0954bbb
pavan-14 <adapalapavan5@gmail.com>
2021-06-07 14:48:50 +0530

    changes

--- Bookstore/testcase/test_views.py
+++ Bookstore/testcase/test_views.py
@@ -4,10 +4,10 @@ class ViewsTestCase(TestCase):
     
     def test_index_loads_properly(self):
         """The index page loads properly"""
-        response = self.client.get('http://*.*.*.*:8080/books/')
+        response = self.client.get('http://3.85.22.38:8080/books/')
         self.assertEqual(response.status_code, 200)
     
     def test_login(self):
-        response = self.client.get('http://*.*.*.*:8080/cuser/signin/')
+        response = self.client.get('http://3.85.22.38:8080/cuser/signin/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cuser/signin.html')
