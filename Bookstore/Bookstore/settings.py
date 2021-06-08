327a031e5682c46d256b7526d7067bec2390f44d
Pavan-14 <84263574+Pavan-14@users.noreply.github.com>
2021-06-08 15:28:27 +0530

    Update settings.py

--- Bookstore/Bookstore/settings.py
+++ Bookstore/Bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '4k=bt$!-jg$94toufi405h_ct48k=xeod@f(u4jpgb%u&cc9^1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.229.140.111',]
+ALLOWED_HOSTS = ['52.86.18.109',]
 
 AUTHENTICATION_BACKENDS = [
 
